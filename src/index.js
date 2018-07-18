059c64f057cfcf1ebeb308f59a038a8f0e96c456
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-18 11:57:28 -0500

    npm exercise

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+const $ = require("jquery");
+$("body").css("background-color", "red");
+
+
+let sayHello = name => {
+    let greeting = "hello";
+    console.log(greeting);
+};
+    sayHello("Linda");
